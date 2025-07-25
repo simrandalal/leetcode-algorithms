f35c2aaeda713707eea1e3030bbc3accb535b0cf
Simran Dalal <147065323+simrandalal@users.noreply.github.com>
2025-07-25 08:57:09 +0530

    Time: 2 ms (80.27%), Space: 45.8 MB (88.54%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int lastNonZeroIndex = 0;
+
+        for (int i=0 ; i < nums.length ; i++){
+            if(nums[i] != 0){
+                nums[lastNonZeroIndex] = nums[i];
+                lastNonZeroIndex++;
+            }
+        }
+
+        for(int i= lastNonZeroIndex; i < nums.length ; i++){
+            nums[i] = 0;
+        }
+
+     
+    }
+}
